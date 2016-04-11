c12299c4ce7edd94b2a4e9a48e3278ffee077412
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-04-10 18:09:45 -0700

    map rendering first layer correctly

--- public/controllers/homeController.js
+++ public/controllers/homeController.js
@@ -4,11 +4,19 @@
   'use strict';
   angular.module('stamplay')
   .controller('homeController', homeController);
-  homeController.$inject = ['Geo','$state',"$http","$scope", "$stamplay",'uiGmapGoogleMapApi'];
+  homeController.$inject = ['Geo','$state',"$http","$scope", "$stamplay",'uiGmapGoogleMapApi','$window'];
 
-  function homeController(Geo, $state, $http, $scope, $stamplay, uiGmapGoogleMapApi){
+  function homeController(Geo, $state, $http, $scope, $stamplay, uiGmapGoogleMapApi, $window){
 
-    Geo.getPlaces().then(function(res){
+    var infowindow;
+    var lat;
+    var lng;
+    var places;
+    var map;
+    var gmarkers = [];
+    var urHere;
+
+    Geo.getPlaces(lat, lng).then(function(res){
       var arr = [];
 
       for(var i = 0; i < res.length; i++){
@@ -17,5 +25,6 @@
       }
       $scope.desc = arr;
     });
+
 }
 })();
--- public/services/Geo.js
+++ public/services/Geo.js
@@ -9,23 +9,35 @@
   function Geo($http, $q, $window) {
 
 	return {
+
     	getPlaces : function(){
+        var lat, lng, map;
       	var q = $q.defer();
-        var map = new google.maps.Map(document.getElementById("map-canvas"));
-        var request = {
-          location:  new google.maps.LatLng("35.1219994", "-118.46876150000001"),
-          radius: 1000,
-          query: "restaurant",
-          rankBy: google.maps.places.RankBy.DISTANCE,
-        };
+        navigator.geolocation.getCurrentPosition(showPosition);
+    
+        function showPosition(position) {
+          lat = position.coords.latitude;
+          lng = position.coords.longitude; 
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: lat, lng: lng},
+            zoom: 15
+          });
 
-        var service = new google.maps.places.PlacesService(map);
-        service.textSearch(request, callback);
+          var request = {
+            location:  new google.maps.LatLng(lat, lng),
+            radius: 1000,
+            query: "restaurant",
+            rankBy: google.maps.places.RankBy.DISTANCE,
+          };
 
-        function callback(results, status) {
-          if (status == google.maps.places.PlacesServiceStatus.OK) {
-            q.resolve(results);
-          }
+          var service = new google.maps.places.PlacesService(map);
+          service.textSearch(request, callback);
+
+          function callback(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+              q.resolve(results);
+            }
+          }        
         }
         return q.promise;
     	},
