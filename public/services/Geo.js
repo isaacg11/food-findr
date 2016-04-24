24561203e187dd1050a24bb49bb293360dabd6e1
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-04-24 15:16:19 -0700

    UI responsive for medium devices - tablets

--- public/services/Geo.js
+++ public/services/Geo.js
@@ -250,7 +250,7 @@
 //INITIALIZE MAP 
           map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: lat, lng: lng},
-            zoom: 16
+            zoom: 14
           });
 //MARKERS - USER
           image = "/public/images/green-marker.png";
